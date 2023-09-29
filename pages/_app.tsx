66d4ef1ef904467938ee4320cc548fd1b509b317
Devon Mars <dbharris2@gmail.com>
2023-09-29 17:28:31 -0600

    Fix TypeScript errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import { ApolloProvider } from '@apollo/client'
 import createApolloClient from "../apollo-client";
 import '../styles/global.css'
+
+type Props = {
+  Component: React.FC,
+  pageProps: any,
+};
  
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: Props) {
   const client = createApolloClient();
   return (
     <ApolloProvider client={client}>
